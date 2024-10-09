6f4e88e24b2c0b3d5fa38a871290137d3c26ca57
aroramrinaal <apply.mrinaal@gmail.com>
2024-10-09 02:56:23 -0700

    ziglings

--- src/001_hello.zig
+++ src/001_hello.zig
@@ -21,14 +21,7 @@ const print = std.debug.print;
 const array = [5]u8 {1,2,3,4,5};
 
 pub fn main() void {
-    // print different types in zig and their memory size
-    print("u8: {}\n", .{@sizeOf(u8)});
-    print("u16: {}\n", .{@sizeOf(u16)});
-    print("u32: {}\n", .{@sizeOf(u32)});
-    print("u64: {}\n", .{@sizeOf(u64)});
-    print("usize: {}\n", .{@sizeOf(usize)});
-    print("i8: {}\n", .{@sizeOf(i8)});
-    print("i16: {}\n", .{@sizeOf(i16)});
-    print("i32: {}\n", .{@sizeOf(i32)});
-    print("i64: {}\n", .{@sizeOf(i64)});
+    print("Hello world!\n", .{});
+    print("array is {d}\n", .{array});
+    print("length of array is {d}\n", .{array.len});
 }
